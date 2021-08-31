7c74422f80955d98a794ffb0ad3aeb2d44837322
BMFreed <badmofo14@gmail.com>
2021-08-31 17:59:32 +0300

    Переработка буллетов
    Заменил псевдоэлементы before у буллетов списка на background-image, т.к. первое решение ломалось при переносе текста на новую строку.

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,7 +1,7 @@
 //Служебный скрипт для позиционирования макета
 
-console.log(
-    "Чтобы правильно наложить макет, сдвиньте его на " +
-        Math.ceil((window.innerWidth - document.documentElement.clientWidth) / 2) +
-        " пикселей влево"
-);
+// console.log(
+//     "Чтобы правильно наложить макет, сдвиньте его на " +
+//         Math.ceil((window.innerWidth - document.documentElement.clientWidth) / 2) +
+//         " пикселей влево"
+// );
