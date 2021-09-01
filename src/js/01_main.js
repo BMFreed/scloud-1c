148c42870f6c99f9ca94ea7ff51b6a1a98c8d3e0
BMFreed <badmofo14@gmail.com>
2021-09-01 09:42:45 +0300

    Обработка выпадающих меню
    Добавил обработчик закрытия выпадающего окна при клике по пункту меню или во вне

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -5,3 +5,29 @@
 //         Math.ceil((window.innerWidth - document.documentElement.clientWidth) / 2) +
 //         " пикселей влево"
 // );
+
+//Обработчик закрытия выпадающего окна при клике по пункту меню или во вне
+
+const flyouts = document.querySelectorAll("details");
+const flyoutContents = document.querySelectorAll("details >ul");
+
+document.addEventListener("click", function (event) {
+    let isClickInside = false;
+    for (let i = 0; i < flyouts.length; i++) {
+        if (flyouts[i].contains(event.target)) {
+            isClickInside = true;
+        }
+    }
+
+    flyouts.forEach(function (item) {
+        if (!isClickInside) {
+            item.open = false;
+        }
+    });
+});
+
+flyoutContents.forEach(function (item) {
+    item.addEventListener("click", function () {
+        item.parentElement.open = false;
+    });
+});
